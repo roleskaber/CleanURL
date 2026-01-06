d4eca7f6cd86139926344500a28a2fa20986c706
Vitaliy <roleskabersupp@gmail.com>
2026-01-07 01:22:53 +0300

    cardinality

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-from fastapi import Body, FastAPI, HTTPException, status
+from fastapi import Body, FastAPI, HTTPException, status, Depends
 from database.db import engine
 from database.models import Base
 from contextlib import asynccontextmanager
+
+
+from ratelimiter.redis import get_redis
+from ratelimiter.service import rate_limiter_factory
+
 from service import generate_short_url, get_url_by_slug
 from fastapi.responses import RedirectResponse
 from exceptions import NoUrlFoundException
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as connection: 
-       await connection.run_sync(Base.metadata.create_all)
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+    redis = get_redis()
+    await redis.ping()
     yield
+    await redis.aclose()
 
 
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/short_url")
+short_url_limiter = rate_limiter_factory("short_url", 5, 5)
+slug_url_limiter = rate_limiter_factory("slug_url", 20, 3)
+
+
+@app.post("/short_url", dependencies=[Depends(short_url_limiter)])
 async def generate_slug_url(
-    long_url: str = Body(embed=True)
+        long_url: str = Body(embed=True)
 ):
     slug = await generate_short_url(long_url)
     return {"slug": slug}
 
 
-@app.get("/{slug}")
+@app.get("/{slug}", dependencies=[Depends(slug_url_limiter)])
 async def redirect_to_url(slug: str):
     try:
         long_url = await get_url_by_slug(slug=slug)
     except NoUrlFoundException:
         return HTTPException(status.HTTP_404_NOT_FOUND, detail="...")
     return RedirectResponse(url=long_url, status_code=status.HTTP_302_FOUND)
-
---
+++ ratelimiter/service.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException, Request, status, Depends
+from typing import Annotated
+
+from ratelimiter.redis import RateLimiter, get_rate_limiter
+
+
+def rate_limiter_factory(
+        endpoint: str,
+        max_requests: int,
+        window_seconds: int,
+):
+    async def dependency(
+            request: Request,
+            rate_limiter: Annotated[RateLimiter, Depends(get_rate_limiter)],
+
+    ):
+        ip_address = request.client.host
+
+        limited = await rate_limiter.is_limited(
+            ip_address,
+            endpoint,
+            max_requests,
+            window_seconds
+        )
+        if limited:
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Too many requests",
+            )
+    return dependency
